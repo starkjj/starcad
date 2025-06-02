b3394a1e551751e237c92837845b0627074f2fce
starkjj <razestudiosdev@gmail.com>
2025-06-02 17:43:50 -0400

    Removed GLM from dependencies (for now) and got ImGui working now.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <SDL3/SDL.h>
 #include <iostream>
-#include <glm/glm.hpp>
+#include "imgui.h"
+#include "imgui_impl_sdl3.h"
+#include "imgui_impl_sdlrenderer3.h"
 
 int main(int argc, char *argv[]) {
 
@@ -9,7 +11,8 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("StarCad", 1040, 768, 0);
+    // I want resolution to 60% of window size
+    SDL_Window* window = SDL_CreateWindow("StarCad", 1280, 720, 0);
     if (window == nullptr) {
         std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
         SDL_Quit();
@@ -17,6 +20,7 @@ int main(int argc, char *argv[]) {
     }
 
     SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+    SDL_SetRenderVSync(renderer, 1);
     if (renderer == nullptr) {
         std::cerr << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
@@ -24,20 +28,39 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    SDL_Event e;
+    // Setup DearImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     bool quit = false;
 
     SDL_FPoint position{};
-
     bool drawing = false;
     SDL_Point start{};
     SDL_Point end{};
 
     while (!quit) {
+        SDL_Event e;
         while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_EVENT_QUIT) {
+            ImGui_ImplSDL3_ProcessEvent(&e);
+
+            if (e.type == SDL_EVENT_QUIT)
                 quit = true;
-            }
+            if (e.type == SDL_EVENT_WINDOW_CLOSE_REQUESTED && e.window.windowID == SDL_GetWindowID(window))
+                quit = true;
 
             if (e.type == SDL_EVENT_MOUSE_MOTION) {
                 position.x = e.motion.x;
@@ -60,6 +83,35 @@ int main(int argc, char *argv[]) {
             }
         }
 
+        // Start the ImGui frame
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+
         // Draw our default background
         SDL_SetRenderDrawColor(renderer, 33, 40, 48, 255);
         SDL_RenderClear(renderer);
@@ -79,8 +131,14 @@ int main(int argc, char *argv[]) {
             SDL_RenderRect(renderer, &rect);
         }
 
+
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
         SDL_RenderPresent(renderer);
     }
+
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
